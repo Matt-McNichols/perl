84dec3bba25cfe2ff18de8831de4d2fd1676febe
Matt-McNichols <mamc0310@colorado.edu>
2015-08-04 15:43:17 -0600

    cleaned pgrep

--- pgrep.pl
+++ pgrep.pl
@@ -1,29 +1,15 @@
 #! /usr/bin/perl
-#
 use warnings;
 use strict;
 
 my $line;
 my $result;
 my $path;
-#my $file_out;
 
-#if($ARGV[0])
-#{
-#  $path=$ARGV[0];
-#  chomp($line=<STDIN>);
-#  print "$line\n";
-#  $result = `find $path -name "*.[vc]" | xargs grep -I -r -m1 "$line" `;
-#  print($result);
-#}
-#else
-#{
-#  chomp($line=<STDIN>);
-  open(my $pgr, '>>', 'pgrep_report.txt');
-  print $pgr "\n\n$ARGV[0]\n";
-  $result = `find .  -name "*.[vc]" | xargs grep -I -r -m1 "$ARGV[0]" `;
+open(my $pgr, '>>', 'pgrep_report.txt');
+print $pgr "\n\n-->$ARGV[0]\n";
+$result = `find .  -name "*.[vc]" | xargs grep -I -r -m1 "$ARGV[0]" `;
 print $pgr $result;
 close $pgr;
 
-#}
 
